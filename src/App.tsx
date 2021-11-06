43d90ce649f175c3f3261e727bad8ca5990fd503
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-06 19:10:39 -0300

    fck

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FormEvent } from react;
+import { useState, useEffect, FormEvent } from 'react';
 import * as C from './App.styles';
 import * as Photos from './services/photos';
 import { Photo } from './types/Photo';
