4489a872df31f797be38e617bc43954c02be891d
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-06 14:41:32 -0300

    fireclean

---
+++ src/libs/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app' ;
+import { getstorage } from 'firebase/storage' ;
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MENSSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: Process.env.REACT_APP_FIREBASE_MEASUREMENTID
+  };
+  
+  
+  const firebaseapp = initializeApp(firebaseConfig);
+  
+  export const storage = getStorage(firebaseapp);
