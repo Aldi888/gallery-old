7f57fde29cd0ce6fdd53004e225f67c33a277d5e
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-06 15:52:22 -0300

    apptest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import * as C from './App.styles';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-    return (
-    <C.Container>
-        ...
-    </C.Container>
-    );   
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
