4f8181653fe2c019d530d4993ac3a7d1bf4e7db6
aldi <40169111+Aldi888@users.noreply.github.com>
2021-11-06 22:12:50 -0300

    pushphoto

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { PhotoItem } from './components';
 
 
 const App = () => {
+  const [uploading, setUploading] = useState(false);
   const [loading, SetLoading] = useState(false);
   const [photos, setPhotos] = useState<Photo[]>([]);
   
@@ -18,8 +19,25 @@ const App = () => {
     getPhotos();
   }, []);
 
-  const handleFormSubmit = () => {
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if(file && file.size > 0) {
+      setUploading(true);
+      let result = await Photos.insert(file);
+      setUploading(false);
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
   }
 
   return (
